5f24dde433d85552394d351389ab0421119daf33
saucesar <saucesarlima2013@gmail.com>
2020-09-22 22:45:34 -0300

    correção no faker

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,9 +3,8 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Product;
-use Faker\Generator as Faker;
 
-$factory->define(Product::class, function (Faker $faker) {
+$factory->define(Product::class, function (Faker\Generator  $faker) {
     return [
         'ref' => "F".$faker->numberBetween(100, 999),
         'name' => $faker->name(),
